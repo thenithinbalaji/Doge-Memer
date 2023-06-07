f15a2c29ad418ba7813a7469df2f4294a7e77f7e
thenithinbalaji <thenithinbalaji@gmail.com>
2023-06-07 14:27:02 +0530

    styled the website

--- server.py
+++ server.py
@@ -4,15 +4,19 @@ from threading import Thread
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-	return render_template("index.html")
+    return render_template("index.html")
 
 
 def run():
-	app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
 
 
 def runserver():
-	thd = Thread(target=run)
-	thd.start()
+    thd = Thread(target=run)
+    thd.start()
+
+
+if __name__ == "__main__":
+    app.run()
